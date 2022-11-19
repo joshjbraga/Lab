795cb230c38561ad3c887143e511da4f0f627436
joshjbraga <98373079+joshjbraga@users.noreply.github.com>
2022-11-18 19:30:27 -0600

    second

--- account.py
+++ account.py
@@ -5,13 +5,25 @@
 class Account:
 
     def __init__(self):
-        self._account_name = 0
-        self._account_balance = 0
+        self._account_name: str = '0'
+        self._account_balance: int = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> bool:
         if amount > 0:
             self._account_balance += amount
             return True
         else:
             return False
 
+    def withdraw(self, amount: int) -> bool:
+        if amount > 0:
+            self._account_balance -= amount
+            return True
+        else:
+            return False
+
+    def get_balance(self) -> str:
+        return f'{self._account_balance}'
+
+    def get_name(self) -> str:
+        return f'{self._account_name}'
