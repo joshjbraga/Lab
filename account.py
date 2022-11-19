cffe96c441bc2dd13e41b7976b0a47ab6941ad75
joshjbraga <98373079+joshjbraga@users.noreply.github.com>
2022-11-18 20:12:37 -0600

    test

--- account.py
+++ account.py
@@ -2,22 +2,40 @@
 # Lab 12
 # 11/18/2022
 
+# Joshua Braga
+# Lab 12
+# 11/18/2022
+
 class Account:
 
-    def __init__(self):
-        self._account_name = 0
-        self._account_balance = 0
+    def __init__(self, name):
+        self._account_name: str = f'{name}'
+        self._account_balance: int = 0
+    '''This function initializes the account'''
 
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> bool:
         if amount > 0:
             self._account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount):
+    '''This function sees if your deposit is a real positive numer
+        and if it is, that amount is added to the account'''
+
+    def withdraw(self, amount: int) -> bool:
         if amount > 0:
             self._account_balance -= amount
             return True
         else:
             return False
+    '''This function sees if your withdrawal is a real positive numer
+        and if it is, that amount is subtracted to the account'''
+
+    def get_balance(self) -> str:
+        return f'{self._account_balance}'
+    '''This function grabs the amount left in the account'''
+
+    def get_name(self) -> str:
+        return f'{self._account_name}'
+    '''This function grabs the name from the account'''
---
+++ test_account.py
@@ -0,0 +1,26 @@
+import pytest
+from account import Account
+
+def test__init__():
+    testName = Account('Jeremy')
+    assert testName.get_name() == 'Jeremy'
+    assert testName.get_balance() == 0
+
+def test_deposit():
+    testName = Account('Jeremy')
+    testName.deposit(-90)
+    assert testName.get_balance() == 0
+    testName.deposit(90)
+    assert testName.get_balance() == 90
+    testName.deposit(0)
+    assert testName.get_balance() == 90
+
+def test_withdarawl():
+    testName = Account('Jeremy')
+    testName.deposit(90)
+    testName.withdraw(-80)
+    assert testName.get_balance() == 0
+    testName.deposit(80)
+    assert testName.get_balance() == 10
+    testName.deposit(0)
+    assert testName.get_balance() == 10
