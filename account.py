476a7ede9260dc53e0db2c8db0fb54630fb06dad
joshjbraga <98373079+joshjbraga@users.noreply.github.com>
2022-11-19 19:10:04 -0600

    ys

--- account.py
+++ account.py
@@ -5,35 +5,54 @@
 
 
 class Account:
+    '''
+    This class is made to create and run an
+    account that you can deposit and withdraw
+    from.
+    '''
 
-    def __init__(self, name):
-        self._account_name: str = f'{name}'
-        self._account_balance: int = 0
-    '''This function initializes the account'''
+    def __init__(self, name: str) -> None:
+        '''
+        This function initializes the account
+        '''
+        self.__account_name: str = f'{name}'
+        self.__account_balance: float = 0
 
-    def deposit(self, amount: int) -> bool:
+
+    def deposit(self, amount: float) -> bool:
+        '''
+        This function sees if your deposit is a real positive numer
+        and if it is, that amount is added to the account
+        '''
         if amount > 0:
-            self._account_balance += amount
+            self.__account_balance += amount
+            print(f'Account balance = {self.__account_balance}')
             return True
         else:
             return False
 
-    '''This function sees if your deposit is a real positive numer
-        and if it is, that amount is added to the account'''
 
-    def withdraw(self, amount: int) -> bool:
+    def withdraw(self, amount: float) -> bool:
+        '''
+        This function sees if your withdrawal is a real positive numer
+        and if it is, that amount is subtracted to the account
+        '''
         if amount > 0:
-            self._account_balance -= amount
+            self.__account_balance = self.__account_balance - amount
+            print(f'Account balance = {self.__account_balance}')
             return True
         else:
             return False
-    '''This function sees if your withdrawal is a real positive numer
-        and if it is, that amount is subtracted to the account'''
 
-    def get_balance(self) -> str:
-        return f'{self._account_balance}'
-    '''This function grabs the amount left in the account'''
+    def get_balance(self) -> float:
+        '''
+        This function grabs the amount left in the account
+        '''
+        balance = self.__account_balance
+        return balance
 
     def get_name(self) -> str:
-        return f'{self._account_name}'
-    '''This function grabs the name from the account'''
+        '''
+        This function grabs the name from the account
+        '''
+        return f'{self.__account_name}'
